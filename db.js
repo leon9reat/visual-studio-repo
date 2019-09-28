e0bb5756766896efd62007d0fc772624e71b1341
leon9reat <mail.buat.forum@gmail.com>
2019-09-28 21:50:51 +0700

    tambah koneksi

---
+++ db.js
@@ -0,0 +1,14 @@
+var mysql = require("mysql");
+
+var conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "cyber123",
+    database: "test"
+});
+
+conn.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = conn;
